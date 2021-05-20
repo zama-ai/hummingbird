361277f461b9f80f58936704ff651596bc02e7b4
Karla Saur <kasaur@microsoft.com>
2021-05-20 20:40:12 +0000

    adding pruned input file

--- tests/errorcode/colserror.py
+++ tests/errorcode/colserror.py
@@ -4,14 +4,23 @@ from hummingbird.ml import constants
 import pandas as pd
 
 m = "/root/hummingbird/tests/errorcode/mymodel.onnx"
-csvstr = "/root/hummingbird/tests/errorcode/X.csv"
-csv = pd.read_csv(csvstr)
+# csvstr = "/root/hummingbird/tests/errorcode/X.csv"
+# csv = pd.read_csv(csvstr)
+
+
+csvstr_pruned = "/root/hummingbird/tests/errorcode/X_pruned.csv"
+csv_20 = pd.read_csv(csvstr_pruned)
+
 
 with open(m, "rb") as binary_file:
     modstr = binary_file.read()
 
 mod = onnx.load_from_string(modstr)
-hbout = convert(mod, "torch", csv)
 
 # this will give an error
-hbout.predict(csv)
+# hbout = convert(mod, "torch", csv)
+# hbout.predict(csv)
+
+
+hbout = convert(mod, "torch", csv_20)
+print(hbout.predict(csv_20))
