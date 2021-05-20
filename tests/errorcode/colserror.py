878e51ac5fb06983cf78dabdb2c393259eed2b7a
Karla Saur <kasaur@microsoft.com>
2021-05-20 19:37:26 +0000

    bugfix

--- tests/errorcode/colserror.py
+++ tests/errorcode/colserror.py
@@ -3,8 +3,8 @@ from hummingbird.ml import convert
 from hummingbird.ml import constants
 import pandas as pd
 
-m = "/root/hummingbird/tests/ravenerror/mymodel.onnx"
-csvstr = "/root/hummingbird/tests/ravenerror/X.csv"
+m = "/root/hummingbird/tests/errorcode/mymodel.onnx"
+csvstr = "/root/hummingbird/tests/errorcode/X.csv"
 csv = pd.read_csv(csvstr)
 
 with open(m, "rb") as binary_file:
